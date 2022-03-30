b9f9b777eca5cb3f794c9f4411eae1f42451ce01
Raziel Cervantes <razielcervantesromero@gmail>
2022-03-30 12:55:46 -0600

    3_display_circle_image

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,10 @@ void main() => runApp(
       MaterialApp(
         home: Scaffold(
           body: Center(
-            child: Container(
-              height: 240,
-              width: 240,
-              child: Image.network(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
                   "https://i2.wp.com/zimbronapps.com/wp-content/uploads/2019/03/flutter.png?fit=750%2C300&ssl=1&resize=350%2C200"),
+              radius: 90,
             ),
           ),
         ),
