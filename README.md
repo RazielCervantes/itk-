4aedcd429801c3b1ff01e6ba86de9f1cc7273a09
Raziel Cervantes <razielcervantesromero@gmail>
2022-04-01 12:05:12 -0600

    12_Row_Google_Login

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Screenshot 
 
- ![screen11](Screenshots/screen_11_row_1_product_card.png)
+ ![screen11](Screenshots/12_Row_Google_Login.png)
--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,33 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 120,
-                width: 120,
-                child: Image.asset("images/1.png"),
-              ),
-              Text(
-                "Learning flutter",
-                style: TextStyle(fontSize: 26),
-              ),
-              IconButton(
-                iconSize: 34,
-                icon: Icon(Icons.thumb_up_alt_outlined),
+          body: Center(
+            child: Container(
+              height: 80,
+              width: 350,
+              child: OutlinedButton(
                 onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: Image.asset("images/3.png"),
+                    ),
+                    Text(
+                      "Continue with Google",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(width: 1.0, color: Colors.black)),
               ),
-            ],
+            ),
           ),
         ),
       ),
